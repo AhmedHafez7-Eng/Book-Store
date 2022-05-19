47acefbdf8f4462bea57d582244fad6939a60daf
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-05-19 21:01:18 +0200

    Build connection & Register configs

--- register.php
+++ register.php
@@ -1,3 +1,40 @@
+<?php
+
+include 'config.php';
+
+if (isset($_POST['submit'])) {
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+    // $user_type = $_POST['user_type'];
+    $user_type = 'user';
+
+    $select_users = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email' OR password = '$pass'") or die("Query failed: " . mysqli_connect_error());
+
+    if (mysqli_num_rows($select_users) > 0) {
+        $message[] = "User Already Exists!";
+        // echo "<script>alert('User already exists');</script>";
+    } else {
+        if ($pass != $cpass) {
+            $message[] = "Confirm password does not match!";
+        } else {
+            $insert_users = mysqli_query($conn, "INSERT INTO users (name, email, password, user_type) VALUES ('$name', '$email', '$pass', '$user_type')") or die("Query failed: " . mysqli_connect_error());
+            if ($insert_users) {
+                $message[] = "User Registered Successfully";
+                // echo "<script>alert('User registered successfully');</script>";
+                header('location: login.php');
+            } else {
+                $message[] = "User Registration Failed";
+                // echo "<script>alert('User registration failed');</script>";
+            }
+        }
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,6 +56,22 @@
 </head>
 
 <body>
+
+    <?php
+
+    if (isset($message)) {
+        foreach ($message as $msg) {
+            echo '
+            <div class="message">
+                <span>' . $msg . '</span>
+                <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+            </div>
+            ';
+        }
+    }
+
+    ?>
+
     <section class="form-container">
         <form action="" method="post">
             <h3>Register Now</h3>
@@ -26,11 +79,11 @@
             <input type="email" name="email" placeholder="Enter Your Email Address" class="box" required>
             <input type="password" name="password" placeholder="Enter A Password" class="box" required>
             <input type="password" name="cpassword" placeholder="Confirm Password" class="box" required>
-            <select name="user_type" required class="box">
+            <!-- <select name="user_type" required class="box">
                 <option value="">Select User Type</option>
                 <option value="admin">Admin</option>
                 <option value="user">User</option>
-            </select>
+            </select> -->
             <input type="submit" name="submit" value="Register" class="btn">
             <p>Already have an account? <a href="login.php">Login from here</a></p>
         </form>
