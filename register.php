4babe3ab2eefc9ab6755cc5fcb729282f0d0f5e1
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-05-19 19:35:24 +0200

    register page structure & styling

--- register.php
+++ register.php
@@ -19,7 +19,22 @@
 </head>
 
 <body>
-
+    <section class="form-container">
+        <form action="" method="post">
+            <h3>Register Now</h3>
+            <input type="text" name="name" placeholder="Enter Your Full Name" class="box" required>
+            <input type="email" name="email" placeholder="Enter Your Email Address" class="box" required>
+            <input type="password" name="password" placeholder="Enter A Password" class="box" required>
+            <input type="password" name="cpassword" placeholder="Confirm Password" class="box" required>
+            <select name="user_type" required class="box">
+                <option value="">Select User Type</option>
+                <option value="admin">Admin</option>
+                <option value="user">User</option>
+            </select>
+            <input type="submit" name="submit" value="Register" class="btn">
+            <p>Already have an account? <a href="login.php">Login from here</a></p>
+        </form>
+    </section>
 </body>
 
 </html>
