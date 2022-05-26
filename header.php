692e414d9cdcdf92cddb673096880b24d832180e
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-05-26 23:14:26 +0200

    Show cart rows number

--- header.php
+++ header.php
@@ -92,7 +92,14 @@ if (isset($_POST['update_profile'])) {
                 <div id="menu-btn" class="fas fa-bars"></div>
                 <a href="search-books.php" class="fas fa-search"></a>
                 <div id="user-btn" class="fas fa-user"></div>
-                <a href="cart.php"><i class="fas fa-shopping-cart"></i> <span>(00)</span></a>
+
+                <?php
+                $select_cart_number = mysqli_query($conn, "SELECT * FROM cart WHERE user_id = '$user_id'") or die("Query failed: " . mysqli_connect_error());
+                $cart_rows_number = mysqli_num_rows($select_cart_number);
+                ?>
+
+                <a href="cart.php"><i class="fas fa-shopping-cart"></i>
+                    <span>(<?php echo $cart_rows_number; ?>)</span></a>
             </div>
 
             <div class="user-box">
