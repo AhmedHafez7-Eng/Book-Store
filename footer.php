eccbbf89aedc7144f206833745cfc465febc8584
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-05-26 23:57:30 +0200

    finish footer

---
+++ footer.php
@@ -0,0 +1,45 @@
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="shop.php">shop</a>
+            <a href="contact.php">contact</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="cart.php">cart</a>
+            <a href="orders.php">orders</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p><i class="fas fa-phone"></i> +20 101-049-5597 </p>
+            <p><i class="fas fa-phone"></i> +20 155-769-6977 </p>
+            <p><i class="fas fa-envelope"></i> ahmedhafezoffic@gmail.com </p>
+            <p><i class="fas fa-map-marker-alt"></i> Mansoura, Dakahlia, Egypt </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/profile.php?id=100005116839262" target="_blank"><i
+                    class="fab fa-facebook-f"></i>
+                Facebook</a>
+            <a href="https://www.linkedin.com/in/ahmedhafez247/" target="_blank"><i class="fab fa-linkedin-in"></i>
+                LinkedIn</a>
+            <a href="https://github.com/AhmedHafez7-Eng" target="_blank"><i class="fab fa-github"></i> Github</a>
+            <a href="https://www.instagram.com/ahmedhafez247/" target="_blank"><i class="fab fa-instagram"></i>
+                Instagram</a>
+            <a href="https://ahmedhafezoffic.netlify.app/" target="_blank"><i class="fas fa-earth-africa"></i>
+                Portfolio</a>
+        </div>
+    </div>
+
+    <p class="credit">created by <a href="https://github.com/AhmedHafez7-Eng" target="_blank">ahmed hafez</a> @
+        <?php echo date('Y'); ?>
+        | all rights reserved! &copy;</p>
+</section>
