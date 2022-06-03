fd8a720e9da658dd05465d5281fd314684fdca26
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-06-03 23:09:15 +0200

    contact page and send message form

--- contact.php
+++ contact.php
@@ -9,6 +9,27 @@ if (!isset($user_id)) {
     header('Location: login.php');
 }
 
+if (isset($_POST['send_message'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $number = $_POST['number'];
+    $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+    $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name='$name' AND email='$email' AND number='$number' AND message='$msg'") or die("Query failed: " . mysqli_connect_error());
+
+    if (mysqli_num_rows($select_message) > 0) {
+        $message[] = 'Message already sent';
+    } else {
+        $insert_message = mysqli_query($conn, "INSERT INTO `message` (user_id,name, email, number, message, msg_status) VALUES ('$user_id','$name', '$email', '$number', '$msg', 'unread')") or die("Query failed: " . mysqli_connect_error());
+        if ($insert_message) {
+            $message[] = 'Message sent successfully';
+        } else {
+            header('Location: contact.php');
+            $message[] = 'Something went wrong, Please try again';
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -34,9 +55,22 @@ if (!isset($user_id)) {
 <body>
     <?php include 'header.php'; ?>
 
+    <div class="heading">
+        <h3>contact us</h3>
+        <p><a href="home.php">home</a> / contact</p>
+    </div>
 
-
-
+    <section class="contact">
+        <form action="" method="post">
+            <h3>get in touch!</h3>
+            <input type="text" name="name" id="name" placeholder="full name" class="box" required>
+            <input type="number" name="number" id="number" placeholder="phone number" class="box" required>
+            <input type="email" name="email" id="email" placeholder="email address" class="box" required>
+            <textarea name="message" id="message" placeholder="your message" cols="30" rows="10" class="box"
+                required></textarea>
+            <input type="submit" name="send_message" value="send message" class="btn">
+        </form>
+    </section>
 
 
     <?php include 'footer.php'; ?>
