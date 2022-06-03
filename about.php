957329a72b8c275dae1f8e41c2d7ff8088e0ec69
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-06-03 20:34:32 +0200

    finish about-page

--- about.php
+++ about.php
@@ -34,10 +34,161 @@ if (!isset($user_id)) {
 <body>
     <?php include 'header.php'; ?>
 
+    <div class="heading">
+        <h3>About Us</h3>
+        <p><a href="home.php">home</a> / about</p>
+    </div>
 
+    <section class="home-about">
+        <!-- <h1 class="title">About EGYBOOK</h1> -->
+        <div class="flex">
+            <div class="imgBx">
+                <img src="img/about-img.jpg" alt="about">
+            </div>
+            <div class="content">
+                <h3>why choose us ?</h3>
+                <p><span>EGY<span>BOOK</span></span> Bookstore is one of the leading book seller in Egypt. Boasting more
+                    than 7,000
+                    Arabic and
+                    foreign
+                    titles and aiming to provide the best book shopping experience.</p>
+                <a href="contact.php" class="white-btn">contact us</a>
+            </div>
+        </div>
+    </section>
 
+    <section class="reviews">
+        <h1 class="title">client's reviews</h1>
 
+        <div class="box-container">
+            <div class="box">
+                <img src="img/pic-1.png" alt="client">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Omar Zaghlol</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-2.png" alt="client">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Laila Ibrahim</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-3.png" alt="client">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Mahmoud Ashraf</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-4.png" alt="client">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Noha Fawzy</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-5.png" alt="client">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+        </div>
+    </section>
 
+    <section class="authors">
+        <h1 class="title">our authors</h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="img/pic-5.png" alt="author">
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-github"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-5.png" alt="author">
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-github"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-5.png" alt="author">
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-github"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-5.png" alt="author">
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-github"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-5.png" alt="author">
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-github"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+            <div class="box">
+                <img src="img/pic-5.png" alt="author">
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                    <a href="#" class="fab fa-github"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div>
+                <h3>Amr Elsayed</h3>
+            </div>
+        </div>
+    </section>
 
     <?php include 'footer.php'; ?>
 
